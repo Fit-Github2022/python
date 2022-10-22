edb258e0a36c957d1f4d347c95110a1af94dd4c4
Fit-Github2022 <115910564+Fit-Github2022@users.noreply.github.com>
2022-10-22 16:25:17 +0800

    Update mymall.py

--- mymall.py
+++ mymall.py
@@ -4,13 +4,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 mc = pd.read_csv('/content/mall_customer.csv')
+
 mc.head()
+
 mc.tail()
+
 mc.describe()
+
 mc.info()
 
 x_mc = mc.drop(['Genre','CustomerID'], axis=1)  
 x_mc
+
 y_mc = mc['Genre']
 y_mc
 
@@ -18,17 +23,23 @@ from sklearn.model_selection import train_test_split
 Xtrain, Xtest, ytrain, ytest = train_test_split(x_mc, y_mc)
 Xtrain.head()
 
+Xtrain.shape #75%
+Xtest.shape #25%
+
 from sklearn.naive_bayes import GaussianNB
 model = GaussianNB()                       
 model.fit(Xtrain, ytrain)                 
 y_model = model.predict(Xtest)
+
 from sklearn.metrics import accuracy_score
 accuracy_score(ytest, y_model)
+
 from sklearn.metrics import classification_report
 print(classification_report(ytest, y_model))
 
 from sklearn.metrics import confusion_matrix 
 confusion_matrix(ytest, y_model)
+
 import matplotlib.pyplot as plt
 from sklearn import metrics
 import numpy as np
