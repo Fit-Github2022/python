589923b7193a5bab5cb1ec5db5a13773d098c7f9
Fit-Github2022 <115910564+Fit-Github2022@users.noreply.github.com>
2022-10-22 16:28:00 +0800

    Update mymall.py

--- mymall.py
+++ mymall.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-mc = pd.read_csv('/content/mall_customer.csv')
+mc = pd.read_csv('mall_customer.csv')
 
 mc.head()
 
